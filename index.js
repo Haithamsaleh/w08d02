37b9e159e96cbab6a28746c0580e4744a6d2fa79
Haithamsaleh <Haitham.saleh.elq.js@tuwaig.edu.sa>
2021-11-29 21:49:43 +0300

    controllers role

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const db = require("./db/index")
 
 
 
+
 const app = express()
 dotenv.config()
 app.use(express.json());
---
+++ routers/controllers/role.js
@@ -0,0 +1,24 @@
+const roleModel = require("./../../db/models/role")
+
+const creatRole = (req, res)=>{
+    const newRole = new roleModel(req.body)
+    newRole
+    .save()
+    .then((result)=>{
+        res.status(200).json(result);
+    })
+    .catch((err)=>{
+        res.status(400).json(err)
+    })
+}
+const Roles = (req, res)=>{
+    roleModel
+    .find({email})
+    .then((result)=>{
+        res.status(200).json(result)
+    })
+    .catch((err)=>{
+        res.status(400).json(err)
+    })
+}
+module.exports={creatRole, Roles}
