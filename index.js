ecd5c956bede07916f1629879d605a08e8b862bd
Haithamsaleh <Haitham.saleh.elq.js@tuwaig.edu.sa>
2021-11-29 19:47:07 +0300

    setup mongoose

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const dotenv =require("dotenv")
+const cors = require("cors")
+const db = require("./db/index")
+
+
+
+
+const app = express()
+dotenv.config()
+app.use(express.json());
+app.use(cors());
+
+
+
+
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, ()=>{
+    console.log(`server on port ${PORT}`);
+})
